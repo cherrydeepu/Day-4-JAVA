9e8439a7671bb229e64a9489d13e86c41402d1ae
cherrydeepu <trishithapandu2218@gmail.com>
2024-12-05 14:26:09 +0530

    Add files via upload

---
+++ ternaryevod.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class  ternaryevod{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        System.out.println((a%2==0)?"Even":"Odd");
+    }
+}
