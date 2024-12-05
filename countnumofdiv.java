ad427b1d685f3e889c8e6be17d95c20539e7a12b
cherrydeepu <trishithapandu2218@gmail.com>
2024-12-05 15:04:45 +0530

    Add files via upload

---
+++ countnumofdiv.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class countnumofdiv{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        for(int j=0;j<t;j++){
+            int n=s.nextInt();
+            int c=0;
+            for(int i=1;i<=n;i++){
+                if(n%i==0){
+                c+=1;
+                }
+            }
+            System.out.println(c);
+        }
+    }
+}
